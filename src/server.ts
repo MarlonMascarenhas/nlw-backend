1459a2d52ef246c73a89cf434e255bd553d05ee5
Marlon Mascarenhas <mmascarenhs@gmail.com>
2020-06-03 17:02:45 -0300

    cors adicionado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import path from 'path';
 
 import routes from './routes';
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
